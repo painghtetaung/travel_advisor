2805232a9c113ff58b3f901914d7682117194ad6
painghtetaung <painghtetaung1999@gmail.com>
2022-01-29 13:35:39 +0630

    google map added

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import {Paper, Typography, useMediaQuery} from '@mui/material';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import Rating from '@mui/material/Rating';
+
+import useStyles from './styles';
 
 const Map = () => {
+	const classes = useStyles();
+	const isMobile = useMediaQuery('(min-width:600px)')
+	const coordinates = { lat: 0, lng: 0};
 
 	return (
-			<h1>hello from Map</h1>
+			<div className={classes.mapContainer}>
+				<GoogleMapReact
+					bootstrapURLKeys={{ key: 'AIzaSyA9L_gg4OZuBD4Zy1dzxQlrs-m9s0NVF7M'}}
+					defaultCenter={coordinates}
+					center={coordinates}
+					defaultZoom={14}
+					margin={[50, 50, 50, 50]}
+					option={''}
+					onChange={''}
+					onChildClick={''}
+				>
+					
+				</GoogleMapReact>
+			</div>
 		);
 };
 
